0518d22eacb63b909c9db32751387888a43ef387
grv <garv.sharma200212@gmail.com>
2025-10-09 17:20:52 +0530

    Add Job component with introductory text and image

---
+++ Career-nest-frontend/src/LandingPage/Job.tsx
@@ -0,0 +1,21 @@
+const Job = () => {
+    return (
+        <div className="flex">
+            <div className="">
+                <div>Find your dream job with us</div>
+                <div>Good life begins with a good company. Start explore thousands of jobs in one place.</div>
+                <div>
+
+                </div>
+            </div>
+
+            <div>
+                <div className="w-[40rem]">
+                    <img src="boy.png" alt="main image" className="mx-auto mt-10" />
+                </div>
+
+            </div>
+        </div>
+    );
+}
+export default Job;
