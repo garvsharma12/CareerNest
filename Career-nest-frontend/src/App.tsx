13592ee217ca89aa5e1711bf27a97531355d8cda
grv <garv.sharma200212@gmail.com>
2025-10-09 15:37:43 +0530

    Enhance Header component with new icons and styles, implement custom theme in MantineProvider

--- Career-nest-frontend/src/App.tsx
+++ Career-nest-frontend/src/App.tsx
@@ -1,11 +1,42 @@
 import './App.css';
-import { MantineProvider } from "@mantine/core";
+import { createTheme, MantineProvider } from "@mantine/core";
 import '@mantine/core/styles.css';
 import HomePage from './Pages/HomePage';
+import { create } from 'domain';
 
 function App() {
+  const theme = createTheme({
+    colors: {
+      'mine-shaft': [
+        '#1c1c1c',
+        '#1a1a1a',
+        '#171717',
+        '#151515',
+        '#121212',
+        '#0f0f0f',
+        '#0c0c0c',
+        '#0a0a0a',
+        '#070707',
+        '#050505',
+      ],
+      'cerise': [
+        '#ff6f91',
+        '#ff5f85',
+        '#ff4f79',
+        '#ff3f6d',
+        '#ff2f61',
+        '#ff1f55',
+        '#ff0f49',
+        '#ff003d',
+        '#e60036',
+        '#cc002e',
+      ],
+    },
+    primaryColor: 'cerise',
+    primaryShade: 5,  
+  })
   return (
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <HomePage />
     </MantineProvider>
   );
--- Career-nest-frontend/src/Header/Header.tsx
+++ Career-nest-frontend/src/Header/Header.tsx
@@ -1,15 +1,15 @@
-import { Avatar } from "@mantine/core";
+import { Avatar, Indicator } from "@mantine/core";
 import {IconCircleDottedLetterC, IconBell, IconSettings } from "@tabler/icons-react";
 
 const Header = () => {
     return <div className="w-full bg-mine-shaft-950 px-6 text-white h-28 flex justify-between items-center">
 
-        <div className="flex gap-3 items-center">
+        <div className="flex gap-3 items-center text-cerise-400">
             <IconCircleDottedLetterC className="h-10 w-10" />
             <div className="text-3xl font-semibold">Career Nest</div>
         </div>
 
-        <div className="flex gap-3">
+        <div className="flex gap-5">
             <a href="">Find Job</a>
             <a href="">Find Talent</a>
             <a href="">Upload Jobs</a>
@@ -17,12 +17,20 @@ const Header = () => {
         </div>
 
         <div className="flex gap-4 items-center">
-            <IconBell/>
             <div className="flex items-center gap-2">
                 <div>Radha</div>
                 <Avatar src="avatar.png" alt="it's me" />
             </div>
-            <IconSettings/>
+
+            <div className="bg-mine-shaft-900 p-2 rounded-md hover:bg-mine-shaft-800 cursor-pointer">
+            <Indicator color="cerise.2" withBorder position="top-end" size={10} offset={4} processing>
+                <IconBell stroke={2.0} />
+            </Indicator>
+            </div>
+
+            <div className="bg-mine-shaft-900 p-2 rounded-md hover:bg-mine-shaft-800 cursor-pointer">
+                <IconSettings stroke={2.0} />
+            </div>
         </div>
     </div>;
 }
