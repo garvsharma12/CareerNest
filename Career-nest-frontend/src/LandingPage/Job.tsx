9602075817e0f3f556f99b1b091a9e8d95dd62e7
grv <garv.sharma200212@gmail.com>
2025-10-10 16:33:45 +0530

    Enhance Job component with updated layout, Avatar integration, and styling updates; extend Tailwind classes for reusable UI elements

--- Career-nest-frontend/src/LandingPage/Job.tsx
+++ Career-nest-frontend/src/LandingPage/Job.tsx
@@ -1,4 +1,4 @@
-import { TextInput } from "@mantine/core";
+import { TextInput, Avatar } from "@mantine/core";
 import { IconSearch } from "@tabler/icons-react";
 const Job = () => {
     return (
@@ -10,7 +10,7 @@ const Job = () => {
                 <div className="text-lg text-mine-shaft-200">
                     Good life begins with a good company. Start explore thousands of jobs in one place.
                 </div>
-                                <div className="flex gap-3 mt-5 items-end">
+                    <div className="flex gap-3 mt-5 items-end">
                     <TextInput className="bg-mine-shaft-800 rounded-md p-1 px-6 text-mine-shaft-100 [&_input]:!text-mine-shaft-100"
                         variant="unstyled"
                         label="Job Title"
@@ -21,7 +21,7 @@ const Job = () => {
                         label="Job Type"
                         placeholder="Full-time"
                         />
-                    <button className="h-17 w-13 flex items-center justify-center rounded-md bg-cerise-500 text-white hover:bg-cerise-400 transition"
+                    <button className="h-17 w-13 flex items-center justify-center rounded-md bg-cerise-500 text-white hover:bg-cerise-400 transition cursor-pointer"
                         type="button"
                         aria-label="Search jobs"
                     >
@@ -33,7 +33,19 @@ const Job = () => {
 
             <div className="w-[55%] flex justify-center items-center">
                 <div className="w-[40rem] transform translate-x-14">
-                    <img src="boy.png" alt="main image" className="mx-auto mt-10" />
+                    <img src="boy.png" alt="boy" className="mx-auto mt-10" />
+                    <div className="absolute top-[39%] w-fit border-2 border-cerise-400 rounded-lg p-2 backdrop-blur-md bg-transparent">
+                        <div className="text-center text-mine-shaft-200">10K+ got jobs</div>
+                        <Avatar.Group>
+                            <Avatar src="image.png" />
+                            <Avatar src="image.png" />
+                            <Avatar src="image.png" />
+                            <Avatar>+9</Avatar>
+                        </Avatar.Group>
+                    </div>
+                    <div>
+                        
+                    </div>
                 </div>
 
             </div>
