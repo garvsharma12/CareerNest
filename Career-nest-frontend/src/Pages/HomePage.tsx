2744c52e6c01abd8360103ce288038084bc7ad99
grv <garv.sharma200212@gmail.com>
2025-10-11 15:06:29 +0530

    Add Companies component to HomePage

--- Career-nest-frontend/src/Pages/HomePage.tsx
+++ Career-nest-frontend/src/Pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import Header from "../Header/Header";
+import Companies from "../LandingPage/Companies";
 import Job from "../LandingPage/Job";
 const HomePage = () => {
     return(
         <div className="min-h-[100vh] bg-mine-shaft-950 font-[poppins]">
             <Header/>
             <Job/>
+            <Companies/>
         </div>
     )
 }
